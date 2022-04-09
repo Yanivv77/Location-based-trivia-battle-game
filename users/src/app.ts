f6d6768da48c2abc391fb283ad880667e3d5cea2
Yanivv77 <Yanivv77@gmail.com>
2022-04-09 16:40:49 +0300

    cros

--- users/src/app.ts
+++ users/src/app.ts
@@ -11,9 +11,13 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import 'dotenv/config' 
 import { userByNameRouter } from './routes/user-by-name';
+const cors = require("cors");
+
+
 
 const app = express();
 app.set('trust proxy', true);
+app.use(cors());
 app.use(json());
 app.use(
   cookieSession({
