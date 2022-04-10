5cf929fd6aed20bb85f02bcd29260156e54e255a
Yakov Gurevich <umanrh@gmail.com>
2022-04-10 11:39:53 +0300

    wtb-29 start and sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import * as React from 'react'
-import MainMenu from './screens/MainMenu'
-import SignupScreen from './screens/SignupScreen'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as React from "react";
+import MainMenu from "./screens/MainMenu";
+import SignupScreen from "./screens/SignupScreen";
+import GameLobbyScreen from "./screens/GameLobbyScreen";
 
 function App() {
   return (
@@ -11,11 +12,12 @@ function App() {
           <Routes>
             <Route path="/" element={<MainMenu />} />
             <Route path="/signup" element={<SignupScreen />} />
+            <Route path="/gamelobby" element={<GameLobbyScreen />} />
           </Routes>
         </div>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/screens/GameLobbyScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GameOptions from '../components/Game/GameOptions'
+import OnlineUsers from '../components/Game/OnlineUsers'
+
+const GameLobbyScreen = () => {
+  return (
+      <>
+    <div>GameLobby</div>
+    <GameOptions/>
+    <OnlineUsers/>
+    </>
+  )
+}
+
+export default GameLobbyScreen
