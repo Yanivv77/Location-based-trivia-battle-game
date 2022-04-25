97e1be3c70007ae1abcd5432f61d5ec18a35549e
Yanivv77 <Yanivv77@gmail.com>
2022-04-26 01:59:04 +0300

    up

---
+++ client/src/components/Helps.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+
+function Helps() {
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          alignItems: 'center',
+          bgcolor: 'background.paper',
+          overflow: 'hidden',
+          borderRadius: '12px',
+          boxShadow: 1,
+          fontWeight: 'bold',
+        }}
+      ></Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          alignItems: 'center',
+          bgcolor: 'background.paper',
+          overflow: 'hidden',
+          borderRadius: '12px',
+          boxShadow: 1,
+          fontWeight: 'bold',
+        }}
+      ></Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          alignItems: 'center',
+          bgcolor: 'background.paper',
+          overflow: 'hidden',
+          borderRadius: '12px',
+          boxShadow: 1,
+          fontWeight: 'bold',
+        }}
+      ></Box>
+    </>
+  )
+}
+
+export default Helps
