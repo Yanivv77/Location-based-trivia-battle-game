52895b83a26efae54af428facc5d34815845d555
Yanivv77 <Yanivv77@gmail.com>
2022-04-26 12:40:07 +0300

    up

--- client/src/components/Helps.jsx
+++ client/src/components/Helps.jsx
@@ -2,32 +2,27 @@ import React from 'react'
 import { LeaderboardSharp as Statistics } from '@mui/icons-material/'
 import { LeaderboardSharp as Follow } from '@mui/icons-material'
 import { LeaderboardSharp as half } from '@mui/icons-material'
+import { Link } from '@mui/material'
 
 function Helps() {
   const IconsArr = [Statistics, Follow, half]
 
   return (
     <>
-    import React from 'react'
-import { Link } from 'react-router-dom'
-export default function Cocktail({ image, name, id, info, glass }) {
-  return (
-    <article className='cocktail'>
-      <div className='img-container'>
-        <img src={image} alt={name} />
-      </div>
-      <div className='cocktail-footer'>
-        <h3>{name}</h3>
-        <h4>{glass}</h4>
-        <p>{info}</p>
-        <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
-          details
-        </Link>
-      </div>
-    </article>
-  )
-}
-    
+      <article className="cocktail">
+        <div className="img-container">
+          <img src={image} alt={name} />
+        </div>
+        <div className="cocktail-footer">
+          <h3>{name}</h3>
+          <h4>{glass}</h4>
+          <p>{info}</p>
+          <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
+            details
+          </Link>
+        </div>
+      </article>
+      ) }
     </>
   )
 }
