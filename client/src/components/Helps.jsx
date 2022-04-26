5e24db0480c29a52f05173f02024b940d1a1cff7
Yanivv77 <Yanivv77@gmail.com>
2022-04-26 12:40:39 +0300

    UP

--- client/src/components/Helps.jsx
+++ client/src/components/Helps.jsx
@@ -7,24 +7,7 @@ import { Link } from '@mui/material'
 function Helps() {
   const IconsArr = [Statistics, Follow, half]
 
-  return (
-    <>
-      <article className="cocktail">
-        <div className="img-container">
-          <img src={image} alt={name} />
-        </div>
-        <div className="cocktail-footer">
-          <h3>{name}</h3>
-          <h4>{glass}</h4>
-          <p>{info}</p>
-          <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
-            details
-          </Link>
-        </div>
-      </article>
-      ) }
-    </>
-  )
+  return <></>
 }
 
 export default Helps
