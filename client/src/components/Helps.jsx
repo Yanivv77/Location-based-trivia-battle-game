ebd2feac4d4f767c54ef6ac59a09325486688ecc
Yanivv77 <Yanivv77@gmail.com>
2022-04-26 12:37:22 +0300

    up

--- client/src/components/Helps.jsx
+++ client/src/components/Helps.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
-
-import { Tooltip } from '@mui/material'
-import { Button } from '@mui/material'
+import { LeaderboardSharp as Statistics } from '@mui/icons-material/'
+import { LeaderboardSharp as Follow } from '@mui/icons-material'
+import { LeaderboardSharp as half } from '@mui/icons-material'
 
 function Helps() {
+  const IconsArr = [Statistics, Follow, half]
+
   return (
     <>
-      <Tooltip title={'longText'}>
-        <Button sx={{ m: 1 }}>Default Width [300px]</Button>
-      </Tooltip>
+    import React from 'react'
+import { Link } from 'react-router-dom'
+export default function Cocktail({ image, name, id, info, glass }) {
+  return (
+    <article className='cocktail'>
+      <div className='img-container'>
+        <img src={image} alt={name} />
+      </div>
+      <div className='cocktail-footer'>
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
+          details
+        </Link>
+      </div>
+    </article>
+  )
+}
+    
     </>
   )
 }
