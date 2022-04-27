c028493cce51569f8c247d5394acc694495be8e9
Yanivv77 <Yanivv77@gmail.com>
2022-04-27 13:48:05 +0300

    fix

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -1,83 +1,75 @@
-import React, { useEffect, useState } from "react";
-import { Grid, Button, Typography, Box, Paper, Stack } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  betweenQuestions,
-  restartGame,
-  finishGame,
-} from "../../features/game/gameSlice";
-import { answerQuestion, nextQuestion } from "../../features/quiz/quizSlice";
-import BetweenQuestionsModal from "../Game/BetweenQuestionsModal";
+import React, { useEffect, useState } from 'react'
+import { Grid, Button, Typography, Box, Paper, Stack } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
+import { betweenQuestions, restartGame, finishGame } from '../../features/game/gameSlice'
+import { answerQuestion, nextQuestion } from '../../features/quiz/quizSlice'
+import BetweenQuestionsModal from '../Game/BetweenQuestionsModal'
+import Helps from '../Helps'
 
 const Game = () => {
-  const [open, setOpen] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(60);
-  const [answers, setAnswers] = useState([]);
+  const [open, setOpen] = useState(false)
+  const [timeLeft, setTimeLeft] = useState(60)
+  const [answers, setAnswers] = useState([])
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const currentQuestion = useSelector((state) =>
     state.quiz.questions[state.quiz.currentQuestionIndex]
       ? state.quiz.questions[state.quiz.currentQuestionIndex]
       : state.quiz.questions[state.quiz.currentQuestionIndex - 1]
-  );
-  const { currentQuestionIndex, score } = useSelector((state) => state.quiz);
+  )
+  const { currentQuestionIndex, score } = useSelector((state) => state.quiz)
 
-  const handleOpen = () => setOpen(true);
+  const handleOpen = () => setOpen(true)
 
-  const handleClose = () => setOpen(false);
+  const handleClose = () => setOpen(false)
 
   const handleAnswer = (answer) => {
-    let nextQuestionTimer;
+    let nextQuestionTimer
 
-    dispatch(answerQuestion({ answer }));
+    dispatch(answerQuestion({ answer }))
 
-    currentQuestionIndex < 9
-      ? (nextQuestionTimer = 4500)
-      : (nextQuestionTimer = 3500);
+    currentQuestionIndex < 9 ? (nextQuestionTimer = 4500) : (nextQuestionTimer = 3500)
 
-    setTimeout(() => handleOpen(), 1500);
+    setTimeout(() => handleOpen(), 1500)
 
-    setTimeout(() => dispatch(nextQuestion()), nextQuestionTimer);
-  };
+    setTimeout(() => dispatch(nextQuestion()), nextQuestionTimer)
+  }
 
   useEffect(() => {
     if (currentQuestionIndex === 10) {
-      dispatch(finishGame());
+      dispatch(finishGame())
     }
-  }, [currentQuestionIndex]);
+  }, [currentQuestionIndex])
 
   useEffect(() => {
-    setAnswers([
-      currentQuestion.correct_answer,
-      ...currentQuestion.incorrect_answers,
-    ]);
-  }, [currentQuestion]);
+    setAnswers([currentQuestion.correct_answer, ...currentQuestion.incorrect_answers])
+  }, [currentQuestion])
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimeLeft((prev) => prev - 1);
-    }, 1000);
+      setTimeLeft((prev) => prev - 1)
+    }, 1000)
 
     return () => {
-      clearInterval(interval);
-    };
-  }, []);
+      clearInterval(interval)
+    }
+  }, [])
 
   return (
     <>
-      {" "}
+      {' '}
       <Button
         variant="contained"
         color="success"
         size="large"
         sx={{ borderRadius: 10, mt: 5 }}
         onClick={() => {
-          dispatch(restartGame());
+          dispatch(restartGame())
         }}
       >
         EXIT GAME
       </Button>
-      <Box sx={{ maxWidth: "400px", m: "0 auto", position: "relative" }}>
+      <Box sx={{ maxWidth: '400px', m: '0 auto', position: 'relative' }}>
         {/* <Typography
           variant="h3"
           sx={{
@@ -94,11 +86,11 @@ const Game = () => {
         <Typography
           variant="h6"
           sx={{
-            textAlign: "center",
+            textAlign: 'center',
 
-            fontWeight: "bold",
-            color: "##eeeeee",
-            position: "absolute",
+            fontWeight: 'bold',
+            color: '##eeeeee',
+            position: 'absolute',
             top: 0,
             right: 0,
           }}
@@ -108,11 +100,11 @@ const Game = () => {
         <Typography
           variant="h6"
           sx={{
-            textAlign: "center",
+            textAlign: 'center',
 
-            fontWeight: "bold",
-            color: "##eeeeee",
-            position: "absolute",
+            fontWeight: 'bold',
+            color: '##eeeeee',
+            position: 'absolute',
             top: 0,
             left: 0,
           }}
@@ -120,7 +112,7 @@ const Game = () => {
           Score : {score}
         </Typography>
         <Box>
-          <Grid container spacing={2} sx={{ width: "100%", mt: 3 }}>
+          <Grid container spacing={2} sx={{ width: '100%', mt: 3 }}>
             <Grid item xs={12}>
               <Paper
                 sx={{
@@ -128,33 +120,30 @@ const Game = () => {
                   pb: 2,
                   mt: 3,
                   mb: 3,
-                  borderRadius: "10px",
-                  minHeight: "200px",
+                  borderRadius: '10px',
+                  minHeight: '200px',
                 }}
               >
                 <Typography
                   variant="h5"
                   sx={{
-                    textAlign: "center",
+                    textAlign: 'center',
 
-                    fontWeight: "bold",
-                    color: "##eeeeee",
+                    fontWeight: 'bold',
+                    color: '##eeeeee',
                     mb: 2,
                   }}
                 >
-                  Question{" "}
-                  <span className={{ fontSize: "15px", color: "red" }}>
-                    {currentQuestionIndex + 1}
-                  </span>
+                  Question <span className={{ fontSize: '15px', color: 'red' }}>{currentQuestionIndex + 1}</span>
                   /10
                 </Typography>
                 <Typography
                   variant="h6"
                   sx={{
-                    textAlign: "center",
+                    textAlign: 'center',
 
-                    fontWeight: "bold",
-                    color: "##eeeeee",
+                    fontWeight: 'bold',
+                    color: '##eeeeee',
                   }}
                 >
                   {currentQuestion.question}
@@ -168,12 +157,12 @@ const Game = () => {
                     variant="contained"
                     size="large"
                     sx={{
-                      minWidth: "150px",
+                      minWidth: '150px',
                       borderRadius: 10,
 
-                      backgroundColor: "secondary.main",
-                      "&:hover": {
-                        backgroundColor: "secondary.dark",
+                      backgroundColor: 'secondary.main',
+                      '&:hover': {
+                        backgroundColor: 'secondary.dark',
                         // opacity: [0.9, 0.8, 0.7],
                       },
                     }}
@@ -184,11 +173,12 @@ const Game = () => {
                 </Grid>
               ))}
           </Grid>
+          <Helps></Helps>
         </Box>
       </Box>
       <BetweenQuestionsModal open={open} handleClose={handleClose} />
     </>
-  );
-};
+  )
+}
 
-export default Game;
+export default Game
--- client/src/screens/GameLobbyScreen.jsx
+++ client/src/screens/GameLobbyScreen.jsx
@@ -1,57 +1,47 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Grid, Button, Typography, Box, Paper } from "@mui/material";
-import TriviaLocationOptions from "../components/Game/TriviaLocation";
-import GameOptions from "../components/Game/GameOptions";
-import LoadingGame from "../components/Game/LoadingGame";
-import Game from "../components/Game/Game";
-import EndGame from "../components/Game/EndGame";
-import BetweenQuestions from "../components/Game/BetweenQuestions";
-import NavBar from "../components/NavBar";
-import LeftSideMenu from "../components/LeftSideMenu";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Grid, Button, Typography, Box, Paper } from '@mui/material'
+import TriviaLocationOptions from '../components/Game/TriviaLocation'
+import GameOptions from '../components/Game/GameOptions'
+import LoadingGame from '../components/Game/LoadingGame'
+import Game from '../components/Game/Game'
+import EndGame from '../components/Game/EndGame'
+import BetweenQuestions from '../components/Game/BetweenQuestions'
+import NavBar from '../components/NavBar'
+import LeftSideMenu from '../components/LeftSideMenu'
 
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux'
 
+import TriviaLocation from './../components/Game/TriviaLocation'
 
-
-import TriviaLocation from "./../components/Game/TriviaLocation";
-import Helps from './../components/Helps';
-
-import {
-  INIT_GAME,
-  LOADING_GAME,
-  GAME_OPTIONS,
-  BETWEEN_QUESTIONS,
-  GAME,
-  END_GAME,
-} from "../utils/gameConstants";
+import { INIT_GAME, LOADING_GAME, GAME_OPTIONS, BETWEEN_QUESTIONS, GAME, END_GAME } from '../utils/gameConstants'
 
 const GameLobbyScreen = () => {
-  const [open, setOpen] = useState(false);
-  const currentStage = useSelector((state) => state.game.stage);
-  const navigate = useNavigate();
-  let gameStage;
+  const [open, setOpen] = useState(false)
+  const currentStage = useSelector((state) => state.game.stage)
+  const navigate = useNavigate()
+  let gameStage
   switch (currentStage) {
     case INIT_GAME:
-      gameStage = <TriviaLocationOptions />;
-      break;
+      gameStage = <TriviaLocationOptions />
+      break
     case GAME_OPTIONS:
-      gameStage = <GameOptions />;
-      break;
+      gameStage = <GameOptions />
+      break
     case LOADING_GAME:
-      gameStage = <LoadingGame />;
-      break;
+      gameStage = <LoadingGame />
+      break
     case GAME:
-      gameStage = <Game />;
-      break;
+      gameStage = <Game />
+      break
     case BETWEEN_QUESTIONS:
-      gameStage = <BetweenQuestions />;
-      break;
+      gameStage = <BetweenQuestions />
+      break
     case END_GAME:
-      gameStage = <EndGame />;
-      break;
+      gameStage = <EndGame />
+      break
     default:
-      break;
+      break
   }
   return (
     <>
@@ -59,11 +49,11 @@ const GameLobbyScreen = () => {
       <Paper
         elevation={3}
         sx={{
-          width: "100%",
-          height: "100%",
+          width: '100%',
+          height: '100%',
           background:
-            "url(https://imgc.allpostersimages.com/img/posters/aged-treasure-map-background_u-L-F78UCO0.jpg?artHeight=900&artPerspective=n&artWidth=900) no-repeat fixed center",
-          backgroundSize: "cover",
+            'url(https://imgc.allpostersimages.com/img/posters/aged-treasure-map-background_u-L-F78UCO0.jpg?artHeight=900&artPerspective=n&artWidth=900) no-repeat fixed center',
+          backgroundSize: 'cover',
           p: 3,
         }}
       >
@@ -74,17 +64,15 @@ const GameLobbyScreen = () => {
           size="large"
           sx={{ borderRadius: 10, mt: 5 }}
           onClick={() => {
-            navigate("/profile");
+            navigate('/profile')
           }}
         >
           Go Back
         </Button>
-
-        <Helps></Helps>
       </Paper>
       <LeftSideMenu open={setOpen} isOpen={open} />
     </>
-  );
-};
+  )
+}
 
-export default GameLobbyScreen;
+export default GameLobbyScreen
