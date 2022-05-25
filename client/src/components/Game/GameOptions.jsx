deded7c22bed0a9124505ba4d737601c999f42ab
Yanivv77 <Yanivv77@gmail.com>
2022-05-25 14:46:25 +0300

    deploy

--- client/src/components/Game/GameOptions.jsx
+++ client/src/components/Game/GameOptions.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { Grid, Button, Typography, Box, Slider } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import MultiUsers from "../Game/MultiUsers";
-import { useDispatch, useSelector } from "react-redux";
-import { initGame, createGame, setTimer } from "../../features/game/gameSlice";
+import React, { useEffect, useState } from 'react'
+import { Grid, Button, Typography, Box, Slider } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import MultiUsers from '../Game/MultiUsers'
+import { useDispatch, useSelector } from 'react-redux'
+import { initGame, createGame, setTimer } from '../../features/game/gameSlice'
 
 const GameOptions = () => {
-  const { secondsPerQuestion } = useSelector((state) => state.game.gameOptions);
-  const [multi, setMulti] = useState(false);
-  const [value, setValue] = useState(secondsPerQuestion || 30);
+  const { secondsPerQuestion } = useSelector((state) => state.game.gameOptions)
+  const [multi, setMulti] = useState(false)
+  const [value, setValue] = useState(secondsPerQuestion || 30)
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const handleChange = (event, newValue) => {
     // setValue(newValue);
-    dispatch(setTimer(newValue));
-  };
+    dispatch(setTimer(newValue))
+  }
 
   useEffect(() => {
-    console.log(secondsPerQuestion);
-  }, [value]);
+    console.log(secondsPerQuestion)
+  }, [value])
   return (
     <>
       {!multi ? (
@@ -27,41 +27,39 @@ const GameOptions = () => {
           <Typography
             variant="h5"
             sx={{
-              textAlign: "center",
-              mt: 2,
+              textAlign: 'center',
+              mt: 1,
 
-              fontWeight: "bold",
-              color: "##eeeeee",
+              fontWeight: 'bold',
+              color: '##eeeeee',
             }}
-          >
-            Choose Game Options
-          </Typography>
+          ></Typography>
           <Button
             variant="contained"
             color="success"
             size="medium"
             sx={{ borderRadius: 10, mb: 1 }}
             onClick={() => {
-              dispatch(initGame());
+              dispatch(initGame())
             }}
           >
             Go Back
           </Button>
-          <Box sx={{ maxWidth: "400px", m: "0 auto" }}>
-            <Grid
-              container
-              spacing={2}
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              sx={{ width: "100%", mt: 3 }}
-            >
+          <Box sx={{ maxWidth: '400px', m: '0 auto' }}>
+            <Grid container spacing={2} direction="column" justifyContent="center" alignItems="center" sx={{ width: '100%', mt: 3 }}>
               <Grid item xs={12}>
                 <Box sx={{ width: 200 }}>
                   <Typography
                     color="primary"
                     id="discrete-slider-custom"
                     gutterBottom
+                    sx={{
+                      textAlign: 'center',
+                      mt: 1,
+
+                      fontWeight: 'bold',
+                      color: '##eeeeee',
+                    }}
                   >
                     {secondsPerQuestion} Seconds Per Question
                   </Typography>
@@ -81,15 +79,25 @@ const GameOptions = () => {
                   variant="contained"
                   color="secondary"
                   size="large"
-                  sx={{ borderRadius: 10, mt: 6, mb: 3 }}
+                  sx={{ borderRadius: 10, mt: 1, mb: 1 }}
                   onClick={() => {
                     // dispatch(fetchQuestions());
-                    dispatch(createGame());
-                    setMulti(true);
+                    dispatch(createGame())
+                    setMulti(true)
                   }}
                 >
-                  Continue
+                  Start Game
                 </Button>
+              </Grid>
+              <Grid container spacing={1} direction="column" justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
+                <Box sx={{ width: 300, height: 250, display: { xs: 'block', sm: 'none', md: 'block' } }}>
+                  <lottie-player
+                    src="https://assets8.lottiefiles.com/packages/lf20_igywev6p.json"
+                    background="transparent"
+                    speed="0.4"
+                    autoplay
+                  ></lottie-player>
+                </Box>
               </Grid>
             </Grid>
           </Box>
@@ -98,7 +106,7 @@ const GameOptions = () => {
         <MultiUsers />
       )}
     </>
-  );
-};
+  )
+}
 
-export default GameOptions;
+export default GameOptions
--- client/src/components/Game/TriviaLocation.jsx
+++ client/src/components/Game/TriviaLocation.jsx
@@ -3,6 +3,8 @@ import { Grid, Button, Typography, Box, Paper } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import NearByModal from '../Game/NearByModal'
 import { useTranslation } from 'react-i18next'
+import AnimatedText from 'react-animated-text-content'
+import { CoolText } from 'react-cool-text'
 
 const TriviaLocation = () => {
   const { t } = useTranslation(['Game/TriviaLocation'])
@@ -14,7 +16,7 @@ const TriviaLocation = () => {
   const handleClose = () => setOpen(false)
   return (
     <>
-      <Button
+      {/*<Button
         variant="contained"
         color="success"
         size="large"
@@ -24,36 +26,68 @@ const TriviaLocation = () => {
         }}
       >
         {t('go back')}
-      </Button>
-      <Box sx={{ maxWidth: '400px', m: '0 auto' }}>
+      </Button>*/}
+      <Box sx={{ maxWidth: '600px', m: '0 auto' }}>
         <Typography
-          variant="h4"
           sx={{
+            fontSize: 38,
             textAlign: 'center',
-            mt: 3,
-            mb: 3,
+            mt: 0,
+            mb: 0,
             fontWeight: 'bold',
-            fontFamily: 'Dollan',
-            color: '##eeeeee',
+            fontFamily: 'Helvetica Neue',
+            color: '#000000',
+            variant: 'h1',
+            display: { xs: 'block' },
           }}
         >
-          {t('to start the game please choose trivia location')}
+          <AnimatedText
+            type="words" // animate words or chars
+            animation={{
+              x: '200px',
+              y: '-20px',
+              scale: 1.7,
+              ease: 'ease-in-out',
+            }}
+            animationType="float"
+            interval={0.06}
+            duration={0.8}
+            tag="p"
+            className="animated-paragraph "
+            includeWhiteSpaces
+            threshold={0.1}
+            rootMargin="20%"
+          >
+            {t(' Hello Choose Trivia Question Location 😃')}
+          </AnimatedText>
         </Typography>
-        <Grid container spacing={2} direction="column" justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
+        <Grid container spacing={1} direction="column" justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
           <Grid item xs={12}>
             <Button variant="contained" size="large" color="secondary" sx={{ borderRadius: 10, mt: 5 }} onClick={handleOpen}>
               {t('near by location')}
             </Button>
           </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" color="secondary" size="large" sx={{ borderRadius: 10, mt: 5 }}>
-              {t('location on map')}
-            </Button>
+
+          <Grid container spacing={1} direction="column" justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
+            <Box sx={{ width: 300, height: 300, display: { xs: 'block', sm: 'none', md: 'block' } }}>
+              <lottie-player
+                src="https://assets8.lottiefiles.com/packages/lf20_tHEtXH.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </Box>
           </Grid>
           <Grid item xs={12}>
-            <Button variant="contained" color="secondary" size="large" sx={{ borderRadius: 10, mt: 3 }}>
+            {/*<Button variant="contained" color="secondary" size="large" sx={{ borderRadius: 10, mt: 5 }}>
+              {t('location on map')}
+            </Button>*/}
+          </Grid>
+          <Grid item xs={12}>
+            {/*<Button variant="contained" color="secondary" size="large" sx={{ borderRadius: 10, mt: 3 }}>
               {t('location from list')}
-            </Button>
+            </Button>*/}
           </Grid>
         </Grid>
         <NearByModal open={open} handleClose={handleClose} />
