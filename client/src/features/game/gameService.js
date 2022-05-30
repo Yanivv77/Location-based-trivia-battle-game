07983903d28e457d22a3404736a19134ccf175bd
Yanivv77 <Yanivv77@gmail.com>
2022-05-31 00:24:24 +0300

    depoy fix

--- client/src/features/game/gameService.js
+++ client/src/features/game/gameService.js
@@ -1,44 +1,44 @@
-import axios from "axios";
+import axios from 'axios'
 
-import quizService from "../quiz/quizService";
-import { v4 as uuid } from "uuid";
+import quizService from '../quiz/quizService'
+import { v4 as uuid } from 'uuid'
 
 //Base URL to Quiz Service
-const API_URL = "https://localhost:5000";
+const API_URL = 'https://worldtrivia.herokuapp.com'
 
 // Create a new Game
 const createGame = async ({ user, invitedPlayers, amount }) => {
-  const host = { ...user, name: `${user.name} (host)` };
+  const host = { ...user, name: `${user.name} (host)` }
   // user.name = `${user.name} (host)`;
   //   const response = await axios.post(API_URL, game);
-  const newGame = {};
-  newGame.id = uuid();
-  newGame.host = host;
-  newGame.gameId = uuid();
-  newGame.questions = await quizService.getQuestions(amount);
-  newGame.currentQuestionNumber = 1;
-  newGame.isActive = false;
-  newGame.isFinished = false;
+  const newGame = {}
+  newGame.id = uuid()
+  newGame.host = host
+  newGame.gameId = uuid()
+  newGame.questions = await quizService.getQuestions(amount)
+  newGame.currentQuestionNumber = 1
+  newGame.isActive = false
+  newGame.isFinished = false
   newGame.invitedPlayers = invitedPlayers?.length
     ? invitedPlayers
     : [
-        { id: uuid(), name: "Yaniv", email: "yaniv@mail.com" },
-        { id: uuid(), name: "Sharon", email: "sharon@mail.com" },
-      ];
-  return newGame;
-};
+        { id: uuid(), name: 'Yaniv', email: 'yaniv@mail.com' },
+        { id: uuid(), name: 'Sharon', email: 'sharon@mail.com' },
+      ]
+  return newGame
+}
 
 const getGameById = async (id) => {
-  const response = await axios.get(API_URL + `/${id}`);
+  const response = await axios.get(API_URL + `/${id}`)
 
-  return response.data.results;
-};
+  return response.data.results
+}
 
 const updateGame = async (id) => {
-  const response = await axios.put(API_URL + `/${id}`);
+  const response = await axios.put(API_URL + `/${id}`)
 
-  return response.data.results;
-};
+  return response.data.results
+}
 //userName, gameId, answers=[], helpersStatus={}
 const addGamePlayer = async (gamePlayer) => {
   const gamePlayerObj = {
@@ -50,23 +50,17 @@ const addGamePlayer = async (gamePlayer) => {
       isStatisticsUsed: false,
       isFolowUsed: false,
     },
-  };
-  const response = await axios.post(
-    API_URL + `/api/gamePlayers`,
-    gamePlayerObj
-  );
+  }
+  const response = await axios.post(API_URL + `/api/gamePlayers`, gamePlayerObj)
 
-  return response.data;
-};
+  return response.data
+}
 
 const updateGamePlayer = async (gamePlayerId, updatedGamePlayer) => {
-  const response = await axios.put(
-    API_URL + `/${gamePlayerId}`,
-    updatedGamePlayer
-  );
+  const response = await axios.put(API_URL + `/${gamePlayerId}`, updatedGamePlayer)
 
-  return response.data;
-};
+  return response.data
+}
 
 const gameService = {
   createGame,
@@ -74,6 +68,6 @@ const gameService = {
   updateGame,
   addGamePlayer,
   updateGamePlayer,
-};
+}
 
-export default gameService;
+export default gameService
