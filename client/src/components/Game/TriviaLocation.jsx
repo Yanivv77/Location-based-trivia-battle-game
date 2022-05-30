688227f56ace0cb9c9baa55bb7fd10d6adc21257
Yanivv77 <Yanivv77@gmail.com>
2022-05-30 23:33:39 +0300

    remove back buttom

--- client/src/components/Game/TriviaLocation.jsx
+++ client/src/components/Game/TriviaLocation.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { Grid, Button, Typography, Box, Paper } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import NearByModal from "../Game/NearByModal";
-import { useTranslation } from "react-i18next";
-import AnimatedText from "react-animated-text-content";
+import React from 'react'
+import { Grid, Button, Typography, Box, Paper } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import NearByModal from '../Game/NearByModal'
+import { useTranslation } from 'react-i18next'
+import AnimatedText from 'react-animated-text-content'
 
 const TriviaLocation = () => {
-  const { t } = useTranslation(["Game/TriviaLocation"]);
+  const { t } = useTranslation(['Game/TriviaLocation'])
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false)
 
-  const navigate = useNavigate();
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const navigate = useNavigate()
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
   return (
     <>
-      <Button
+      {/* <Button
         variant="contained"
         color="success"
         size="medium"
@@ -25,30 +25,30 @@ const TriviaLocation = () => {
         }}
       >
         {t("go back")}
-      </Button>
-      <Box sx={{ maxWidth: "400px", m: "0 auto", mb: 4 }}>
+      </Button> */}
+      <Box sx={{ maxWidth: '400px', m: '0 auto', mb: 4 }}>
         <Typography
           component="div"
           variant="h1"
           sx={{
             fontSize: 38,
-            textAlign: "center",
+            textAlign: 'center',
             mt: 0,
             mb: 0,
-            fontWeight: "bold",
-            fontFamily: "Helvetica Neue",
-            color: "#000000",
+            fontWeight: 'bold',
+            fontFamily: 'Helvetica Neue',
+            color: '#000000',
 
-            display: { xs: "block" },
+            display: { xs: 'block' },
           }}
         >
           <AnimatedText
             type="words" // animate words or chars
             animation={{
-              x: "200px",
-              y: "-20px",
+              x: '200px',
+              y: '-20px',
               scale: 1.7,
-              ease: "ease-in-out",
+              ease: 'ease-in-out',
             }}
             animationType="float"
             interval={0.06}
@@ -59,26 +59,13 @@ const TriviaLocation = () => {
             threshold={0.1}
             rootMargin="20%"
           >
-            {t("hello choose trivia questions location")}
+            {t('hello choose trivia questions location')}
           </AnimatedText>
         </Typography>
-        <Grid
-          container
-          spacing={1}
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ width: "100%" }}
-        >
+        <Grid container spacing={1} direction="column" justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
           <Grid item xs={12}>
-            <Button
-              variant="contained"
-              size="large"
-              color="secondary"
-              sx={{ borderRadius: 10, mt: 5 }}
-              onClick={handleOpen}
-            >
-              {t("near by location")}
+            <Button variant="contained" size="large" color="secondary" sx={{ borderRadius: 10, mt: 5 }} onClick={handleOpen}>
+              {t('near by location')}
             </Button>
           </Grid>
 
@@ -87,7 +74,7 @@ const TriviaLocation = () => {
               sx={{
                 width: 300,
                 height: 300,
-                display: { xs: "block", sm: "none", md: "block" },
+                display: { xs: 'block', sm: 'none', md: 'block' },
               }}
             >
               <lottie-player
@@ -113,7 +100,7 @@ const TriviaLocation = () => {
         <NearByModal open={open} handleClose={handleClose} />
       </Box>
     </>
-  );
-};
+  )
+}
 
-export default TriviaLocation;
+export default TriviaLocation
